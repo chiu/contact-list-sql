65505cf8fd34122a2d272c744735ee97b79605e3
chiu <vchiucode@gmail.com>
2015-05-12 23:22:07 +0000

    working version 2

--- contact.rb
+++ contact.rb
@@ -1,25 +1,13 @@
-require 'pg'
-require 'dotenv'
-require 'uri'
-Dotenv.load
+require_relative 'database'
 
 class Contact
-  uri = URI.parse(ENV['DATABASE_URL'])
-  @@postgres = PG.connect(uri.hostname, uri.port, nil, nil, uri.path[1..-1], uri.user, uri.password)
-# Output all author records from the authors table
-puts "getting contacts..."
-@@postgres.exec( "SELECT * FROM contacts" ) do |results|
-  # results is a collection (array) of records (hashes)... Nice!
-  results.each do |contact_element|
-   # puts contact_element.inspect
- end
-end
+
 
 attr_accessor :firstname, :lastname, :email 
 attr_reader :id
 
 def postgres
-  @@postgres
+  @@postgres = Database.connect
 end
 
 def initialize(firstname, lastname, email, id=nil)
@@ -79,32 +67,7 @@ end
     results
   end
 
-  def self.find_univeral(column_name, value)
-    results = []
-    puts column_name
-    puts query_string = "SELECT id, firstname, lastname, email FROM contacts WHERE #{column_name} = $1"
-    @@postgres.exec_params(query_string, [value]) do |rows|
-      rows.each do |row|
-        results << Contact.new(row['firstname'],row['lastname'],row['email'],row['id'])
-      end
-    end
-    results
-  end
-
-
-
-  def self.find_all_by_firstname(firstname_value)
-   self.find_univeral('firstname', firstname_value)
- end
-
- def self.find_all_by_email(email_value)
-  self.find_univeral('email', email_value)
-end
-
-
-def self.find_all_by_lastname(lastname_value)
- self.find_univeral('lastname', lastname_value)
-end
+  
 
 
 
--- main.rb
+++ main.rb
@@ -1,32 +1,33 @@
 #main.rb
-require_relative '2_contact_list_database'
+require_relative 'contact'
+require_relative 'database'
 require 'pry'
 
 
 # arvinder = Instructor.new('Arvinder', 1000)
 # arvinder.save
 
- charlie = Contact.new('Charlie', 'Bennett', 'charlie@gmail.com')
- charlie.save
+#  charlie = Contact.new('Charlie', 'Bennett', 'charlie@gmail.com')
+#  charlie.save
 
 
-drake = Contact.new('Drake', 'Darrington', 'drakeydrake@gmail.com')
-drake.save
+# drake = Contact.new('Drake', 'Darrington', 'drakeydrake@gmail.com')
+# drake.save
 
-drake.destroy
-
-
-coolguy = Contact.find(2)
-
-#puts coolguy.inspect  
+# drake.destroy
 
 
-all_people = Contact.all
-#puts all_people.inspect
+# coolguy = Contact.find(2)
+
+# #puts coolguy.inspect  
 
 
-anders = Contact.find_all_by_lastname('Anders')
-#puts anders.inspect
+# all_people = Contact.all
+# #puts all_people.inspect
+
+
+# anders = Contact.find_all_by_lastname('Anders')
+# #puts anders.inspect
 
 
 puts "start test"
@@ -46,7 +47,7 @@ email_clones2 = Contact.find_all_by_email('drakeydrake@gmail.com')
 puts email_clones2.inspect
 
 
-puts "universal test"
+ puts "universal test"
 
-anders = Contact.find_all_by_lastname('Anders')
-puts anders.inspect
+anders = Database.find_all_by_lastname('Anders')
+ puts anders.inspect
