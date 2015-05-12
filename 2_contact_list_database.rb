6e8e62eb53ae32c9302dc4e8f02ee5061f92e013
chiu <vchiucode@gmail.com>
2015-05-12 22:26:18 +0000

    working build 2

--- 2_contact_list_database.rb
+++ 2_contact_list_database.rb
@@ -3,22 +3,20 @@ require 'dotenv'
 require 'uri'
 Dotenv.load
 
-
 class Contact
-uri = URI.parse(ENV['DATABASE_URL'])
-@@postgres = PG.connect(uri.hostname, uri.port, nil, nil, uri.path[1..-1], uri.user, uri.password)
+  uri = URI.parse(ENV['DATABASE_URL'])
+  @@postgres = PG.connect(uri.hostname, uri.port, nil, nil, uri.path[1..-1], uri.user, uri.password)
 # Output all author records from the authors table
-puts "getting authors ..."
+puts "getting contacts..."
 @@postgres.exec( "SELECT * FROM contacts" ) do |results|
   # results is a collection (array) of records (hashes)... Nice!
-  results.each do |author|
-    puts author.inspect
-  end
+  results.each do |contact_element|
+   # puts contact_element.inspect
+ end
 end
 
 attr_accessor :firstname, :lastname, :email 
 attr_reader :id
-
 
 def postgres
   @@postgres
@@ -39,16 +37,15 @@ end
 def save
   if is_new?
     result = postgres.exec_params('INSERT INTO contacts (firstname, lastname, email) VALUES ($1, $2, $3) returning id', [@firstname, @lastname, @email])
-      #puts result.inspect
-      @id = result[0]['id']
-    else
-      postgres.exec_params('UPDATE contacts SET firstname = $1, lastname = $2, email = $3 WHERE id = $4', [@firstname, @lastname, @email, @id])
-    end
+    @id = result[0]['id']
+  else
+    postgres.exec_params('UPDATE contacts SET firstname = $1, lastname = $2, email = $3 WHERE id = $4', [@firstname, @lastname, @email, @id])
   end
+end
 
-  def destroy
-    postgres.exec_params('DELETE FROM contacts WHERE id = $1', [@id])
-  end
+def destroy
+  postgres.exec_params('DELETE FROM contacts WHERE id = $1', [@id])
+end
 
   ## DANGER Below is wet wet code. It's up to you to DRY it out and make it more succinct.
   
@@ -72,7 +69,22 @@ def save
     @@postgres.exec_params('SELECT id, firstname, lastname, email FROM contacts') do |rows|
       rows.each do |row|
         results << Contact.new(
-           row['firstname'],
+         row['firstname'],
+         row['lastname'],
+         row['email'],
+         row['id']
+         )
+      end
+    end
+    results
+  end
+
+  def self.find_all_by_lastname(lastname)
+    results = []
+    @@postgres.exec_params('SELECT id, firstname, lastname, email FROM contacts WHERE lastname = $1', [lastname]) do |rows|
+      rows.each do |row|
+        results << Contact.new(
+          row['firstname'],
           row['lastname'],
           row['email'],
           row['id']
@@ -81,6 +93,41 @@ def save
     end
     results
   end
+
+
+  def self.find_all_by_firstname(firstname)
+    results = []
+    @@postgres.exec_params('SELECT id, firstname, lastname, email FROM contacts WHERE firstname = $1', [firstname]) do |rows|
+      rows.each do |row|
+        results << Contact.new(
+          row['firstname'],
+          row['lastname'],
+          row['email'],
+          row['id']
+          )
+      end
+    end
+    results
+  end
+
+
+  def self.find_all_by_email(email)
+    results = []
+    @@postgres.exec_params('SELECT id, firstname, lastname, email FROM contacts WHERE email = $1', [email]) do |rows|
+      rows.each do |row|
+        results << Contact.new(
+          row['firstname'],
+          row['lastname'],
+          row['email'],
+          row['id']
+          )
+      end
+    end
+    results
+  end
+
+
+
 
   # def self.where_coolness_above(coolness)
   #   results = []
