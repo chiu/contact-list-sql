6f469ad91cb90dfb2fd8840080ab81e08232099a
chiu <chiucode@gmail.com>
2015-05-14 01:11:03 +0000

    cool

--- contact.rb
+++ contact.rb
@@ -3,9 +3,20 @@
 class Contact < ActiveRecord::Base
 
 
+ def self.find_all_by_lastname(name_argument)
+   result = []
+   binding.pry
+   result << where(lastname:  name_argument)
+ end
 
+ def self.find_all_by_firstname(name_argument)
+   result = []
+   result << where(firstname:  name_argument)
+ end
 
-  
-
+ def self.find_all_by_email(name_argument)
+   result = []
+   result << where(email:  name_argument)
+ end
 
 end
--- main.rb
+++ main.rb
@@ -13,3 +13,65 @@ john_attributes = {firstname: 'John', lastname: 'Smith', email: 'johnsmith@gmail
 john = Contact.new(john_attributes)
 puts john.inspect
 john.save
+
+
+
+tyler_attributes = {firstname: 'Tyler', lastname: 'Smith', email: 'tylersmith@gmail.com'}
+tyler = Contact.new(tyler_attributes)
+puts tyler.inspect
+tyler.save
+
+
+john_attributes = {firstname: 'John', lastname: 'Smith', email: 'johnsmith@gmail.com'}
+john = Contact.new(john_attributes)
+puts john.inspect
+john.save
+
+
+
+tyler_attributes = {firstname: 'Tyler', lastname: 'Smith', email: 'tylersmith@gmail.com'}
+tyler = Contact.new(tyler_attributes)
+puts tyler.inspect
+tyler.save
+
+
+
+
+john_attributes = {firstname: 'John', lastname: 'Smith', email: 'johnsmith@gmail.com'}
+john = Contact.new(john_attributes)
+puts john.inspect
+john.save
+
+
+
+tyler_attributes = {firstname: 'Tyler', lastname: 'Smith', email: 'tylersmith@gmail.com'}
+tyler = Contact.new(tyler_attributes)
+puts tyler.inspect
+tyler.save
+
+
+
+tyler_attributes = {firstname: 'Tyler', lastname: 'Rex', email: 'tylersmith@gmail.com'}
+tyler = Contact.new(tyler_attributes)
+puts tyler.inspect
+tyler.save
+
+
+
+
+puts "find test"
+tyler_found = Contact.find(2)
+
+puts tyler_found.inspect
+
+puts "find by last name"
+puts Contact.find_all_by_lastname('Rex').inspect
+
+
+puts "find by first name"
+puts Contact.find_all_by_firstname('John').inspect
+
+
+puts "find by email"
+puts Contact.find_all_by_email('tylersmith@gmail.com').inspect
+
